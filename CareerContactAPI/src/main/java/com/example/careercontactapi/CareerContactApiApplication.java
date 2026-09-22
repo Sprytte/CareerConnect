be2831fd56a881424f675c37aaa39ede4ef57f43
Emilie Parent <lolabff@hotmail.com>
2026-09-21 21:36:24 -0400

    Create Springboot project

---
+++ CareerContactAPI/src/main/java/com/example/careercontactapi/CareerContactApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.careercontactapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerContactApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerContactApiApplication.class, args);
+    }
+
+}
