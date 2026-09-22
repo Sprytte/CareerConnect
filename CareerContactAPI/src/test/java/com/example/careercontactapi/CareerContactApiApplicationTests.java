dc4a530edc94acee8fce1bfb78cefa136474319d
Emilie Parent <lolabff@hotmail.com>
2026-09-21 21:36:24 -0400

    Create Springboot project

---
+++ CareerContactAPI/src/test/java/com/example/careercontactapi/CareerContactApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.careercontactapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerContactApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
